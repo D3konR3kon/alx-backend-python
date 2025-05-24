f477aa3a0ef24bc515e8b1406e8132c9e86d2f38
d3konr3kon <dilavuyo@outlook.com>
2025-05-24 20:18:16 +0200

    log queries

---
+++ python-generators-0x01/0-log_queries.py
@@ -0,0 +1,43 @@
+import sqlite3
+import functools
+
+
+def log_queries(func):
+    """
+    Decorator that logs SQL queries executed by database functions.
+    """
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'query' in kwargs:
+            query = kwargs['query']
+        elif args and isinstance(args[0], str):
+            query = args[0]
+        else:
+            query = None
+            for arg in args:
+                if isinstance(arg, str) and any(keyword in arg.upper() for keyword in ['SELECT', 'INSERT', 'UPDATE', 'DELETE']):
+                    query = arg
+                    break
+        
+    
+        if query:
+            print(f"[SQL QUERY LOG] Executing: {query}")
+        else:
+            print(f"[SQL QUERY LOG] Executing function: {func.__name__}")
+        
+        result = func(*args, **kwargs)
+        
+        return result
+    
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+users = fetch_all_users(query="SELECT * FROM users")
