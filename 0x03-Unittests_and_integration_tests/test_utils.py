bf1481f44c7ae9e3e30bd32d2c9a27a3dc9ff3e6
Vuyo Dila <103577606+D3konR3kon@users.noreply.github.com>
2025-05-30 00:56:55 +0200

    fixed typo!

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,10 +20,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
-    def test_access_nestesd_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(self, nested_map, path):
         """Test that access_nested_map raises KeyError for invalid paths."""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
             
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
