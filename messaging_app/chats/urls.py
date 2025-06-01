a12da54e29ac34c8e46b4080f513cf2da07fcdaf
Vuyo Dila <103577606+D3konR3kon@users.noreply.github.com>
2025-06-01 02:55:48 +0200

    4. Set up url routing

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
+router = routers. NestedDefaultRouter()
+router.register(r'conversations', ConversationViewSet, lookup='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
