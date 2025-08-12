7031d112ae02707723652fc67e23e2d875a709f9
Vuyo Dila <103577606+D3konR3kon@users.noreply.github.com>
2025-08-12 13:53:27 +0200

    Registeres RolepermissionMiddleware class to middlewares

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -163,7 +163,21 @@ REST_FRAMEWORK = {
     ],
 }
 
-
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
+    'chats.middleware.RolepermissionMiddleware',
+    
+    
+]
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
